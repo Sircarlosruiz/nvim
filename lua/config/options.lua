3373dfdcf07d60cdc3be448a7c1f9dc540ebe96c
Carlos Ruiz <52216208+Sircarlosruiz@users.noreply.github.com>
2025-07-02 13:11:38 -0600

    feat(options): set shell based on os

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+
+-- Detect the operating system and configure the default shell
+local sysname = vim.loop.os_uname().sysname
+if sysname == "Windows_NT" then
+  -- Use PowerShell on Windows for shell commands
+  vim.opt.shell = "pwsh"
+else
+  -- Use the user's shell if defined, otherwise fall back to bash
+  vim.opt.shell = os.getenv("SHELL") or "/bin/bash"
+end
